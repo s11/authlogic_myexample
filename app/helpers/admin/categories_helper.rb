46f356c5f4920b0b4817aa96704fdbe1ba009bc4
unknown <sjoshi@.mrccoffshore.local>
2011-02-16 16:14:41 +0530

    delete included

--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -5,6 +5,8 @@ module Admin::CategoriesHelper
       if category.parent_id.blank?
         ret += "<li>"
         ret += link_to category.name, :action => 'edit', :id => category
+        ret += " | "
+        ret += link_to 'Destroy', admin_category_path(category), :confirm => 'Are you sure?', :method => :delete
         ret += find_all_subcategories(category) if category.children.size > 0
         ret += "</li>"
       end
@@ -18,12 +20,16 @@ module Admin::CategoriesHelper
     category.children.each { |subcat|
       if subcat.children.size > 0
         ret += '<li>'
-        ret += link_to h(subcat.name), :action => 'edit', :id => subcat 
+        ret += link_to h(subcat.name), :action => 'edit', :id => subcat
+        ret += " | "
+        ret += link_to 'Destroy', admin_category_path(subcat), :confirm => 'Are you sure?', :method => :delete
         ret += find_all_subcategories(subcat)
         ret += '</li>'
       else
         ret += '<li>'
         ret += link_to h(subcat.name), :action => 'edit', :id => subcat
+        ret += " | "
+        ret += link_to 'Destroy', admin_category_path(subcat), :confirm => 'Are you sure?', :method => :delete
         ret += '</li>'
       end
     }
