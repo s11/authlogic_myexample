1b5fb3a6e1529691868b5d69c7a78ece1c92a74d
unknown <sjoshi@.mrccoffshore.local>
2011-02-16 17:29:55 +0530

    override

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,3 +1,13 @@
 class Admin::CategoriesController < InheritedResources::Base
   ###All the methods are generated automatically
+
+  ### override the create method to redirect to index page
+  def create
+    create! { admin_categories_path}
+  end
+
+  ### override the update method to redirect to index page
+  def update
+    update! {admin_categories_path}
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
+  config.gem "authlogic", :version => '2.1.6'
   config.gem 'inherited_resources', :version => '1.0.6'
   config.gem 'awesome_nested_set', :version => '1.4.3'
   # Only load the plugins named here, in the order given (default is alphabetical).
