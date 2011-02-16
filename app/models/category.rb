845eac19a97278aacdfdcb4781fcbe6582005503
unknown <sjoshi@.mrccoffshore.local>
2011-02-16 17:52:21 +0530

    column addition....

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   acts_as_nested_set
+  validates_presence_of :name, :description
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110215042832) do
+ActiveRecord::Schema.define(:version => 20110216121320) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110215042832) do
     t.integer  "rgt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "sessions", :force => true do |t|
